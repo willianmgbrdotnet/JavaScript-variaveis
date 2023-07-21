ce62ec2aab89183667fdab3f70f7c0ce9e7a5215
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-07-21 16:53:35 -0300

    const objeto

--- README.md
+++ README.md
@@ -13,4 +13,5 @@ Scopo restrito. Só pode ser acessada no bloco "{}" onde foi declarada.
 # const 
 
 Assim como o "let", só pode ser acessada no mesmo scopo onde foi declarada. Com a diferença de que não pode ser alterada no scopo onde foi declarada.
+No caso da constante ser um objeto (ou um array), sua propriedade pode ser alterada, mantendo a mesma instância. (Veja o código)
 
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const a = 30;
 
-//Dentro do mesmo scopo, uma vez declarada não pode ser alterada. Ocorre o erro: "Identifier 'a' has already been declared"
-/*const a = 40;
-console.log(a); */
+/*
+if (true)
+{
+    const a = { nome : "joão" };
+    const a = { nome : "josé" };
 
-    //Assim como o "let", só pode ser acessada dentro do mesmo Scopo.
-    if(true){
-        const a = 35;
-        const a = 50;
-    
-        if(true){
-            //Dentro do mesmo scopo, uma vez declarada não pode ser alterada. Ocorre o erro: "Identifier 'a' has already been declared"
-            console.log(a);
-    
-        }
-    }
+    //Ocorre o mesmo erro porquê 'a' já foi declarada neste scopo.
+    console.log(a);
+}*/
+
+//Neste caso funciona. O objeto instanciado continua sendo o mesmo, o que mudou foi sua propriedade.
+const a = { nome : 'maria'};
+a.nome = 'sara';
+
+console.log(a);
