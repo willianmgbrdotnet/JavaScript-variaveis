7a91403b97dda262b658ad68fd5380f2a19dc988
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-07-21 16:34:38 -0300

    const 2

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const a = 30;
 
+//Dentro do mesmo scopo, uma vez declarada não pode ser alterada. Ocorre o erro: "Identifier 'a' has already been declared"
+/*const a = 40;
+console.log(a); */
+
     //Assim como o "let", só pode ser acessada dentro do mesmo Scopo.
-if(true){
-    const a = 35
-
     if(true){
-        //Também funciona. Acessada dentro de um bloco que está dentro do bloco em que a variável foi declarada.
-        console.log(a);
-
+        const a = 35;
+        const a = 50;
+    
+        if(true){
+            //Dentro do mesmo scopo, uma vez declarada não pode ser alterada. Ocorre o erro: "Identifier 'a' has already been declared"
+            console.log(a);
+    
+        }
     }
-}
