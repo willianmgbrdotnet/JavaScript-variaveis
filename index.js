ff1e45e532ab02383ae46bdf74295465b2bf1ef0
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-07-21 14:42:29 -0300

    let 2

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-
+let a = 25;
 
 if(true){
-    let a = 20;
-    //Acessando a variável dentro do bloco em que foi declarada.
-    console.log(a);
+    //A variável pode ser alterada.
+    let a = 15.6
 
+    if(true){
+        //Também funciona. Acessada dentro de um bloco que está dentro do bloco em que a variável foi declarada.
+        console.log(a);
+
+    }
 }
-//Acessando a variável fora do bloco em que a mesma foi declarada.
-//Usando "let" ocorre o erro: "a is not defined"
-//console.log(a); 
